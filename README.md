efd2c50fe1a50b1ac6991836e2843a2d68517ee3
Antti Salo <antti.salo@sc5.io>
2018-09-06 16:03:03 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Gatsby + Strapi front
---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        contentTypes: [
+          'article',
+          'user'
+        ]
+      }
+    }
+  ],
+}
